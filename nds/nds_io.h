ea849f941fcfbda177a15e31e8b6d91379299fd2
unknown <skouhei@gmail.com>
2008-12-30 19:33:10 +0900

    test frame buffer mode.

--- nds/nds_io.h
+++ nds/nds_io.h
@@ -21,7 +21,7 @@ typedef	volatile s32      vs32;
 #define	NDS_RAM     0x02000000
 #define	NDS_IOBASE  0x04000000
 #define NDS_PALETTE 0x05000000
-#define	NDS_VRAM    0x06000000
+#define	NDS_VRAM    0x06800000 /* Frame Buffer Mode */
 
 #define GBA_IWRAM_DATA
 
